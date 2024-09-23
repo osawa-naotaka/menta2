6191d6720086fd3ce7de6eef2e1d83040a83637a
osawa-naotaka <ohsawa.naotaka@gmail.com>
2024-09-23 12:29:13 +0900

    0923-2

--- wp-content/themes/menta2/assets/js/app.js
+++ wp-content/themes/menta2/assets/js/app.js
@@ -1,1 +1,16 @@
 console.info("reading theme JavaScript.");
+
+function tabClick(tabheader)
+{
+    ["tab-web", "tab-marketing", "tab-sns", "tab-consulting"].forEach(
+        x => {
+            const classes = document.getElementsByClassName(x)[0].classList
+            if(x == tabheader.value) {
+                classes.add("show-tab");
+                classes.remove("hide-tab");
+            } else {
+                classes.remove("show-tab");
+                classes.add("hide-tab");
+            }
+        });
+}
